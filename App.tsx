193953321621805cefa6afd580caa7261711f6dc
Yamikur0 <68624487+Yamikur0@users.noreply.github.com>
2022-07-19 02:23:27 +0700

    update

--- App.tsx
+++ App.tsx
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import axios from "axios";
-import DropDownPicker from 'react-native-dropdown-picker';
+import DropDownPicker, { ItemType, ValueType } from 'react-native-dropdown-picker';
 
-type ProvinceData = {
+type ProvinceData = ValueType & {
   code: string;
   name: string;
+
 }
 
-type DistrictData = {
+type DistrictData = ValueType & {
   code: string;
   name: string;
   province: string;
 }
 
-type CommuneData = {
+type CommuneData = ValueType & {
   code: string;
   name: string;
   district: string;
@@ -26,33 +27,67 @@ DropDownPicker.setListMode("SCROLLVIEW");
 
 export default function App() {
 
-  const [province, setprovince] = useState([]);
-  const [district, setdistrict] = useState([]);
-  const [commune, setcommune] = useState([]);
+  const [province, setProvince] = useState<ItemType<ProvinceData>[]>([]);
+  const [district, setDistrict] = useState<ItemType<DistrictData>[]>([]);
+  const [commune, setCommune] = useState<ItemType<CommuneData>[]>([]);
+
+  //Custom Dropdown Picker for province
+  const [provinceValue, setProvinceValue] = useState<ProvinceData>({} as ProvinceData);
+  const [provinceOpen, setProvinceOpen] = useState(false);
+  const onCountryOpen = useCallback(() => {
+    setDistrictOpen(false);
+    setCommuneOpen(false);
+  }, []);
+
+  //Custom Dropdown Picker for district
+  const [districtValue, setDistrictValue] = useState<DistrictData>({} as DistrictData);
+  const [districtOpen, setDistrictOpen] = useState(false);
+  const onDistrictOpen = useCallback(() => {
+    setProvinceOpen(false);
+    setCommuneOpen(false);
+  }, []);
+
+  //Custom Dropdown Picker for commune
+  const [communeValue, setCommuneValue] = useState<CommuneData>({} as CommuneData);
+  const [communeOpen, setCommuneOpen] = useState(false);
+  const onCommuneOpen = useCallback(() => {
+    setProvinceOpen(false);
+    setDistrictOpen(false);
+  }, []);
 
   useEffect(() => {
     axios.get('https://api.mysupership.vn/v1/partner/areas/province').then((response) => {
-      setprovince(response.data.results);
-      console.log(response.data.results);
+      setProvince(response.data.results);
     });
   }, []);
 
   useEffect(() => {
-    // if () {
+    if (provinceValue.code) {
+      axios.get('https://api.mysupership.vn/v1/partner/areas/district?', {
+        params: {
+          province: province.find((item) => item.value?.code)
+        }
+      }).then((response) => {
+        setDistrict(response.data.results);
+      });
+      console.log(province.find((item) => item.value?.code));
+      
+    }
+    console.log(provinceValue.toString());
+    
+  }, [provinceValue]);
 
-    // }
-    axios.get('https://api.mysupership.vn/v1/partner/areas/district?', {
-      params: {
-        province: '87' //.../district?province=87
-      }
-    }).then((response) => {
-      setdistrict(response.data.results);
-      console.log(response.data.results);
-    });
-  }, []);
-
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(null);
+  useEffect(() => {
+    if (districtValue.code) {
+      axios.get('https://api.mysupership.vn/v1/partner/areas/commune?', {
+        params: {
+          district: districtValue.code 
+        }
+      }).then((response) => {
+        setCommune(response.data.results);
+      });
+    }
+  }, [districtValue]);
 
   return (
     <View style={styles.container}>
@@ -62,11 +97,42 @@ export default function App() {
           label: 'name',
           value: 'name'
         }}
-        open={open}
-        value={value}
+        open={provinceOpen}
+        onOpen={onCountryOpen}
+        value={provinceValue}
         items={province}
-        setOpen={setOpen}
-        setValue={setValue}
+        setOpen={setProvinceOpen}
+        setValue={setProvinceValue}
+        onChangeValue={(item) => {
+          console.log(item);
+          
+        }}
+      />
+      <DropDownPicker
+        placeholder="Select district"
+        schema={{
+          label: 'name',
+          value: 'name'
+        }}
+        open={districtOpen}
+        onOpen={onDistrictOpen}
+        value={districtValue}
+        items={district}
+        setOpen={setDistrictOpen}
+        setValue={setDistrictValue}
+      />
+      <DropDownPicker
+        placeholder="Select commune"
+        schema={{
+          label: 'name',
+          value: 'name'
+        }}
+        open={communeOpen}
+        onOpen={onCommuneOpen}
+        value={communeValue}
+        items={commune}
+        setOpen={setCommuneOpen}
+        setValue={setCommuneValue}
       />
       <StatusBar style="auto" />
     </View>
